b6510cabb78a60918f1518346f2de37b89805a0c
BulatZ <zambulat@yandex.ru>
2019-06-01 14:25:18 +0300

    First commit

---
+++ WindowsFormsApp1/CitiesDBContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace WindowsFormsApp1
+{
+    public class CitiesDBContext : DbContext
+    {
+
+        public DbSet<City> Cities { get; set; }
+        public CitiesDBContext()
+            : base("DefaultConnection")
+        { }
+
+    }
+}
---
+++ WindowsFormsApp1/SampleData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class SampleData
+    {
+        public static void EditCity(CitiesDBContext dbContext)
+        {
+            if (!dbContext.Cities.Any())
+            {
+                var firstCity = new City { Name = "Казань" };
+                var secondCity = new City { Name = "Новосибирск" };
+                var thirdCity = new City { Name = "Ульяновск" };
+                var fourthCity = new City { Name = "Набережные Челны" };
+                var fifthCity = new City { Name = "Уфа" };
+
+                dbContext.Cities.Add(firstCity);
+                dbContext.Cities.Add(secondCity);
+                dbContext.Cities.Add(thirdCity);
+                dbContext.Cities.Add(fourthCity);
+                dbContext.Cities.Add(fifthCity);
+
+                dbContext.SaveChanges();
+            }            
+        }
+    }
+}
